ba3da9a126117f95c61d0165bf8061c35c28d668
Jack Ault <henlowgg@outlook.com>
2023-12-12 17:59:42 -0500

    default function parameters solution

---
+++ ES6/Set Default Parametes for Your Functions/DefaultFunctionParameters.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
+
+
+// multiple declarations can be presented within the first set of clappers ()
+
+//  however it can be done like (a, b = 1, c = 2 + 4)
