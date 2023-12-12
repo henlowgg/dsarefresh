2c432eded90d3656220887ef9e09945decabe945
Jack Ault <henlowgg@outlook.com>
2023-12-12 15:02:42 -0500

    generating whole numbers

--- Generate Random Whole Numbers With JS/RandomWhole.js
+++ Generate Random Whole Numbers With JS/RandomWhole.js
@@ -1,3 +1,17 @@
 function randomWholeNum() {
     return Math.random();
   }
+
+//   return a random whole number in the range from 0-9
+
+function randomWholeNum() {
+    return Math.floor(Math.random() * 10);
+}
+
+console.log(randomWholeNum);
+
+// basically 0-9 means just use 10 when writing out Math.random() * x
+
+// same thing will go for everthing else when calling Math.random(), then multiplying
+// the result by 20, then passing the value to Math.floor() to round the value down to the nearest whole number
+
