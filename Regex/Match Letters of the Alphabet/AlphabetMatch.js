09d0ba0846abf5600a554144a58e315b4e65dd09
Jack Ault <henlowgg@outlook.com>
2023-12-13 16:16:35 -0500

    solution

---
+++ Regex/Match Letters of the Alphabet/AlphabetMatch.js
@@ -0,0 +1,8 @@
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
+
+// can match characters by using the hyphen character i.e., [a-e] would give you all letters from a through e , including themselves
+
+// im just going to use [a-z] for sake of being lazy so, 
+
