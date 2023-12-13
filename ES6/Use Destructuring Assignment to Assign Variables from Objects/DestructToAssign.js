a6626a5f34f66d98a755f0dbb3f11b0720016743
Jack Ault <henlowgg@outlook.com>
2023-12-13 11:34:43 -0500

    solution

--- ES6/Use Destructuring Assignment to Assign Variables from Objects/DestructToAssign.js
+++ ES6/Use Destructuring Assignment to Assign Variables from Objects/DestructToAssign.js
@@ -6,7 +6,15 @@ const HIGH_TEMPERATURES = {
   
   // Only change code below this line
     
-  const highToday = HIGH_TEMPERATURES.today;
-  const highTomorrow = HIGH_TEMPERATURES.tomorrow; 
+//   const highToday = HIGH_TEMPERATURES.today;
+//   const highTomorrow = HIGH_TEMPERATURES.tomorrow; 
   
+const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+
   // Only change code above this line
+
+  // solution just needs the value of today and to assign it to highToday, and same for tomorrow: highTomorrow
+
+  // same shit to do syntax wise with const { shit: bigShit, shitted: littleShit } = NO_MORE_COFFEE
+
+//   just for reference ...
