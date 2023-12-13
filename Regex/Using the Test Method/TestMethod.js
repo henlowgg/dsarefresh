c302d3da780680cec79ab070e00c5e1b571a23dd
Jack Ault <henlowgg@outlook.com>
2023-12-13 15:46:13 -0500

    more commenting

--- Regex/Using the Test Method/TestMethod.js
+++ Regex/Using the Test Method/TestMethod.js
@@ -3,3 +3,5 @@ let myRegex = /Hello/;
 let result = myRegex.test(myString); // Change this line
 
 // just add .test(myString) to the end of myRegex
+
+//ok i see, so its bascially just testing myString to see if it has the word Hello in it, i.e., myRegex.test(myString) tests if hello is in myString
