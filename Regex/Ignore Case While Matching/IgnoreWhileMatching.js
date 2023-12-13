bf262fef3e50e3cb3d3b142435465b00ab346616
Jack Ault <henlowgg@outlook.com>
2023-12-13 15:55:10 -0500

    solution for ignoring case while matching

---
+++ Regex/Ignore Case While Matching/IgnoreWhileMatching.js
@@ -0,0 +1,9 @@
+let myString = "freeCodeCamp";
+let fccRegex = /change/; // Change this line
+let result = fccRegex.test(myString);
+
+
+// most important thing to note is just that simply adding i after // makes it so that when searching with regex you can ignore upper, camel, or lower case
+
+
+// so for example /change/i would search for any instance of 'change' regardless of it's casing
