3d7512911c14c02c615bb396920116bd969286ec
Jack Ault <henlowgg@outlook.com>
2023-12-13 15:59:25 -0500

    solution for extract matches

---
+++ Regex/Extract Matches/ExtractMatches.js
@@ -0,0 +1,7 @@
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(codingRegex); // Change this line
+
+//two ways to write it out, can either import the codingRegex to the .match() method or simply write the word /coding/ so i mean either way is fine, but using the variable keyword name is better
+
+
