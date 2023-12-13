f8cf4d9bca3a8eb278c07c677d619ab823888a26
Jack Ault <henlowgg@outlook.com>
2023-12-13 16:26:01 -0500

    setup

---
+++ Regex/Match Single Characters Not Specified/MatchingNotSpecified.js
@@ -0,0 +1,8 @@
+let quoteSample = "3 blind mice.";
+let myRegex = /change/; // Change this line
+let result = myRegex; // Change this line
+
+
+// bascially can match all characters that are not explicitly specified
+
+// /[^aeiou0-9]/ig
