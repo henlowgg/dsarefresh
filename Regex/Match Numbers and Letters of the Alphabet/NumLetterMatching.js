6861564bb0710808cef166fd4b9fe0a45e065bfd
Jack Ault <henlowgg@outlook.com>
2023-12-13 16:20:50 -0500

    setup

---
+++ Regex/Match Numbers and Letters of the Alphabet/NumLetterMatching.js
@@ -0,0 +1,12 @@
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /change/; // Change this line
+let result = myRegex; // Change this line
+
+
+// the search functionality using a hyphen is not limited to only letters
+
+// can use numbers too and can also use them in the same area in general 
+
+
+// /[a-z0-9]/gi would give ya just that, searches for all letters and all numbers globally and without case prejudice
+
