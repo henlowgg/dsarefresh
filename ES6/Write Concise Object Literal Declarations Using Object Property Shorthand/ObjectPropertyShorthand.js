beac37060b8adb23c22bcb0c0c9fcc7a27bdf1ea
Jack Ault <henlowgg@outlook.com>
2023-12-13 14:02:42 -0500

    solution for object property shorthand to write concise object literal declarations

---
+++ ES6/Write Concise Object Literal Declarations Using Object Property Shorthand/ObjectPropertyShorthand.js
@@ -0,0 +1,17 @@
+// const createPerson = (name, age, gender) => {
+//     // Only change code below this line
+//     return {
+//       name: name,
+//       age: age,
+//       gender: gender
+//     };
+//     // Only change code above this line
+//   };
+
+//   keep return, just basically shorten it up and use the ({}) syntax for writing it out
+
+// SOLUTION
+
+const createPerson = (name, age, gender) => {
+    return ({name, age, gender});
+};
