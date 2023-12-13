6da12f949ed8dadf53181e8c886420ee7ab8c897
Jack Ault <henlowgg@outlook.com>
2023-12-13 11:48:34 -0500

    solution for destruct arrays, and setup for destructuring rest elements

--- ES6/Use Destructuring Assignment to Assign Variables from Arrays/DestructToAssignArrays.js
+++ ES6/Use Destructuring Assignment to Assign Variables from Arrays/DestructToAssignArrays.js
@@ -1,2 +1,7 @@
 let a = 8, b = 6;
 // Only change code below this line
+
+[a, b] = [b, a];
+
+// all you need to do is write out a, b and swap them, and they'll read the position relative to the current array
+
