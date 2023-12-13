fe88db0d2ed6215aaee478221c625cabc422d4cd
Jack Ault <henlowgg@outlook.com>
2023-12-13 14:54:18 -0500

    solution

---
+++ ES6/Create a JavaScript Promise/Promise.js
@@ -0,0 +1,9 @@
+const makeServerRequest = new Promise((resolve, reject) => {
+
+});
+
+
+
+// to create the new promise itself, you need to declare makeServerRequest
+
+// the function passed in is written as const makeServerRequest = new Promise((resolve, reject) => {});
