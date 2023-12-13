e08476f12a9652fdb7550ed6189bae96cb786fa5
Jack Ault <henlowgg@outlook.com>
2023-12-13 14:06:21 -0500

    solution for concise declarative functions

--- ES6/Write Concise Declarative Functions with ES6/ConciseDeclarativeFunctions.js
+++ ES6/Write Concise Declarative Functions with ES6/ConciseDeclarativeFunctions.js
@@ -1,10 +1,24 @@
 // Only change code below this line
+// const bicycle = {
+//     gear: 2,
+//     setGear: function(newGear) {
+//       this.gear = newGear;
+//     }
+//   };
+//   // Only change code above this line
+//   bicycle.setGear(3);
+//   console.log(bicycle.gear);
+
+//   this one simple, just remove the : function portion of the code and it will be correctly refactored
+
+// SOLUTION
+
 const bicycle = {
     gear: 2,
-    setGear: function(newGear) {
-      this.gear = newGear;
+    setGear(newGear) {
+        this.gear = newGear;
     }
-  };
-  // Only change code above this line
-  bicycle.setGear(3);
-  console.log(bicycle.gear);
+};
+
+bicycle.setGear(3);
+console.log(bicycle.gear);
