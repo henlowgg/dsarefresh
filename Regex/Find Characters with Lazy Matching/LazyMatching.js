859562b5509379894c38b85f3a5af0f47be8c890
Jack Ault <henlowgg@outlook.com>
2023-12-13 16:46:28 -0500

    solution added

---
+++ Regex/Find Characters with Lazy Matching/LazyMatching.js
@@ -0,0 +1,8 @@
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<.*?>/; // Change this line
+let result = text.match(myRegex);
+
+
+// just add a question mark to the end of the *
+
+// answer is /<.*?>/
