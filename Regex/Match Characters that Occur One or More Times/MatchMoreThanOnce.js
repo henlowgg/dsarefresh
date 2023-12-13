4c2d458d14a32b71b3051f5e451ff07125f9cb0d
Jack Ault <henlowgg@outlook.com>
2023-12-13 16:33:43 -0500

    solution

--- Regex/Match Characters that Occur One or More Times/MatchMoreThanOnce.js
+++ Regex/Match Characters that Occur One or More Times/MatchMoreThanOnce.js
@@ -1,3 +1,8 @@
 let difficultSpelling = "Mississippi";
-let myRegex = /change/; // Change this line
+let myRegex = /s+/g; // Change this line
 let result = difficultSpelling.match(myRegex);
+
+// using the + character can check multiple times in a single word for a character or grouping of characters 
+
+
+//only have to write /s+/g for this to find all iterations for the letter s with the global flag attached
