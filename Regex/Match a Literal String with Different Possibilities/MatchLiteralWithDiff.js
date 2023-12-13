bcc594c259873a868a2a08973fd401c43fbda1e3
Jack Ault <henlowgg@outlook.com>
2023-12-13 15:52:18 -0500

    solution

---
+++ Regex/Match a Literal String with Different Possibilities/MatchLiteralWithDiff.js
@@ -0,0 +1,6 @@
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
+
+
+// remove 'change' in line 2, then add dog|cat|bird|fish in its place
