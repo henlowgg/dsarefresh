e528824d3ced1c49d206eb1f4e344e8d707bcd65
Jack Ault <henlowgg@outlook.com>
2023-12-13 14:14:22 -0500

    solution

--- ES6/Use class Syntax to Define a Constructor Function/DefineConstructors.js
+++ ES6/Use class Syntax to Define a Constructor Function/DefineConstructors.js
@@ -1,6 +1,20 @@
 // Only change code below this line
-
+class Vegetable {
+    constructor(name){
+        this.name = name;
+    }
+}
 // Only change code above this line
 
 const carrot = new Vegetable('carrot');
 console.log(carrot.name); // Should display 'carrot'
+
+
+// using 'this'
+
+// use constructor method
+
+//  constructor(n){
+//     this.n = n;
+//  }
+// }
