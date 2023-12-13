5e3a1e6beece0286b717543f8495dc40fd3f8d8c
Jack Ault <henlowgg@outlook.com>
2023-12-13 16:38:28 -0500

    solution

---
+++ Regex/Match Characters that Occur Zero or More Times/MatchCharactersZeroTimes.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+let chewieRegex = /change/; // Change this line
+// Only change code above this line
+
+let result = chewieQuote.match(chewieRegex);
+
+
+// only thing to do here is to add /Aa*/
+
+// this will search for all occurances of A and a within the string
+
+
+// important to note that the * symbol needs to be at the end of the letters instead of how we have ^ and + at the beginning 
