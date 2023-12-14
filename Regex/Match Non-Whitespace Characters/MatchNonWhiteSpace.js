86d3d0c380cdba341fa5aeba6b0ef6bd22210c72
Jack Ault <henlowgg@outlook.com>
2023-12-14 17:43:54 -0500

    solution

--- Regex/Match Non-Whitespace Characters/MatchNonWhiteSpace.js
+++ Regex/Match Non-Whitespace Characters/MatchNonWhiteSpace.js
@@ -1,3 +1,9 @@
 let sample = "Whitespace is important in separating words";
-let countNonWhiteSpace = /change/; // Change this line
+let countNonWhiteSpace = /\S/g; // Change this line
 let result = sample.match(countNonWhiteSpace);
+
+
+// add capital \S with a global flag for solution, 
+
+
+// is a shortcut for this basically /[^\r\t\f\n\v]/
