b941ddd0ce3b040fc061e324b33853e4d9f43a12
Jack Ault <henlowgg@outlook.com>
2023-12-14 17:10:50 -0500

    solution

--- Regex/Match Ending String Patterns/MatchingEndStringPatterns.js
+++ Regex/Match Ending String Patterns/MatchingEndStringPatterns.js
@@ -1,3 +1,5 @@
 let caboose = "The last car on a train is the caboose";
-let lastRegex = /change/; // Change this line
+let lastRegex = /caboose$/; // Change this line
 let result = lastRegex.test(caboose);
+
+// just add caboose$ in the search section, is just searching for the word at the end of the string
