27ac0b81642adc6169bc9aa664ee758c63861c4e
Jack Ault <henlowgg@outlook.com>
2023-12-14 17:40:28 -0500

    solution

--- Regex/Match Whitespace/MatchWhitespace.js
+++ Regex/Match Whitespace/MatchWhitespace.js
@@ -1,3 +1,5 @@
 let sample = "Whitespace is important in separating words";
-let countWhiteSpace = /change/; // Change this line
+let countWhiteSpace = /\s/g; // Change this line
 let result = sample.match(countWhiteSpace);
+
+// just add lowercase \s in with a global flag
