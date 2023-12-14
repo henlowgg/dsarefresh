ee7c229e5f99893704e6fda5b440fa9ab12cbe10
Jack Ault <henlowgg@outlook.com>
2023-12-14 17:23:57 -0500

    solution

--- Regex/Match All Numbers/MatchAllNum.js
+++ Regex/Match All Numbers/MatchAllNum.js
@@ -1,3 +1,9 @@
 let movieName = "2001: A Space Odyssey";
-let numRegex = /change/; // Change this line
+let numRegex = /\d/g; // Change this line
 let result = movieName.match(numRegex).length;
+
+// so just lowercase d with a forward slash
+
+// theres a pattern for shortcuts to just use forward slash before whatever letter
+
+// looks like this /\d/g
