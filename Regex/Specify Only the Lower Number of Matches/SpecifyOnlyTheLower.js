2840cf8077dbbe9bac62b4e33f88796b044f26ad
Jack Ault <henlowgg@outlook.com>
2023-12-14 17:55:07 -0500

    solution

--- Regex/Specify Only the Lower Number of Matches/SpecifyOnlyTheLower.js
+++ Regex/Specify Only the Lower Number of Matches/SpecifyOnlyTheLower.js
@@ -1,3 +1,6 @@
 let haStr = "Hazzzzah";
-let haRegex = /change/; // Change this line
+let haRegex = /Hazz{3,}ah/; // Change this line
 let result = haRegex.test(haStr);
+
+
+// tried to be tricky but you have to have 1 less than the number your parameters are giving for example if you want 4 or more times, your have to use {3,} 
