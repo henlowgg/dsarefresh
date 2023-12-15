94be9ebb7e590d68bff36c1e7f8a440ba5d3f72b
Jack Ault <henlowgg@outlook.com>
2023-12-15 15:44:17 -0500

    setup for shift and pop

---
+++ Basic Data Structures/Remove Items from an Array with pop() and shift()/PopAndShift.js
@@ -0,0 +1,7 @@
+function popShift(arr) {
+    let popped; // Change this line
+    let shifted; // Change this line
+    return [shifted, popped];
+  }
+  
+  console.log(popShift(['challenge', 'is', 'not', 'complete']));
