17102f628f8f756c6485401e733345dad40ab534
Jack Ault <henlowgg@outlook.com>
2023-12-15 01:50:13 -0500

    solution

---
+++ Debugging/Prevent Infinite Loops with a Valid Terminal Condition/PreventGojo.js
@@ -0,0 +1,9 @@
+function myFunc() {
+    for (let i = 1; i <= 4; i += 2) {
+      console.log("Still going!");
+    }
+  }
+
+  // just dont break your sht with running the example for forever 
+
+  // just change != to <= 
