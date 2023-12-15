9f4067c0a1c1ace05427ededf4752728a41acd7b
Jack Ault <henlowgg@outlook.com>
2023-12-15 02:01:32 -0500

    solution

---
+++ Basic Data Structures/Access an Array's Contents Using Bracket Notation/AccessContents.js
@@ -0,0 +1,9 @@
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "Some new shit instead of b"
+// Only change code above this line
+console.log(myArray);
+
+
+// just write myArray[1] = "some shit"; to solve it
+// everything starts at the zeroth index [0]
