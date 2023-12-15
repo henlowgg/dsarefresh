f4782b922493992063bc81029c8e6ca231a280c0
Jack Ault <henlowgg@outlook.com>
2023-12-15 01:19:41 -0500

    setup and solution, just add the ===

---
+++ Debugging/Catch Use of Assignment Operator Instead of Equality Operator/CatchAssignmentOp.js
@@ -0,0 +1,14 @@
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if(x === y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result);
+
+// just make the if statement correctly equal for x and y
+// if(x===y) {}
