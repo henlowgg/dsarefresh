5433b921b2b99a3d9a29162902afc48e59b82f6e
Jack Ault <henlowgg@outlook.com>
2023-12-14 20:21:29 -0500

    solution

--- Debugging/Understanding the Differences between the freeCodeCamp and Browser Console/DifferentConsoles.js
+++ Debugging/Understanding the Differences between the freeCodeCamp and Browser Console/DifferentConsoles.js
@@ -1,1 +1,4 @@
 let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+
+
+// just use console.log(output) and console.clear() to clear all the bs in the console
