4a5348b33ae994f41f63ac38c263c8db3f53a7c4
Jack Ault <henlowgg@outlook.com>
2023-12-15 01:16:03 -0500

    solution

--- Debugging/Catch Mixed Usage of Single and Double Quotes/MixedSignals.js
+++ Debugging/Catch Mixed Usage of Single and Double Quotes/MixedSignals.js
@@ -1,2 +1,6 @@
-let innerHtml = "<p>Click here to <a href="#Home">return home</a></p>";
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
 console.log(innerHtml);
+
+// just change the double quotes "" after href to use single ''
+
+// or vice versa and just wrap the whole thing in single quotes, (my pref is single)
