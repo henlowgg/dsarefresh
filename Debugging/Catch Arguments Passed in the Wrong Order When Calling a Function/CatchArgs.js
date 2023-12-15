e8e769577905027495ff590a101f66055c57b077
Jack Ault <henlowgg@outlook.com>
2023-12-15 01:30:12 -0500

    solution

--- Debugging/Catch Arguments Passed in the Wrong Order When Calling a Function/CatchArgs.js
+++ Debugging/Catch Arguments Passed in the Wrong Order When Calling a Function/CatchArgs.js
@@ -4,5 +4,9 @@ function raiseToPower(b, e) {
   
   let base = 2;
   let exp = 3;
-  let power = raiseToPower(exp, base);
+  let power = raiseToPower(base, exp);
   console.log(power);
+
+  // dumb, just wants you to change the order of base and exp
+
+  // solution is raiseToPower(base, exp)
