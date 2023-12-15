92f13cacda0319acb5f4a7d7fda1d4caf56ba6a9
Jack Ault <henlowgg@outlook.com>
2023-12-15 01:12:50 -0500

    solution

--- Debugging/Catch Unclosed Parentheses, Brackets, Braces and Quotes/CatchStuff.js
+++ Debugging/Catch Unclosed Parentheses, Brackets, Braces and Quotes/CatchStuff.js
@@ -1,3 +1,6 @@
-let myArray = [1, 2, 3;
-    let arraySum = myArray.reduce((previous, current =>  previous + current);
+let myArray = [1, 2, 3];
+    let arraySum = myArray.reduce((previous, current) =>  previous + current);
     console.log(`Sum of array values is: ${arraySum}`);
+
+    // add a ] at the end of line 1
+    // add a ) after the first "current", so its a proper .reduce() method
