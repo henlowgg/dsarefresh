6b3267378ff60fe98fb7cb4655bb944ad2a5e7fb
Jack Ault <henlowgg@outlook.com>
2023-12-14 19:28:48 -0500

    solution

--- Regex/Check For Mixed Grouping of Characters/MixedGroup.js
+++ Regex/Check For Mixed Grouping of Characters/MixedGroup.js
@@ -1,4 +1,18 @@
 let myString = "Eleanor Roosevelt";
-let myRegex = /False/; // Change this line
+let myRegex = /(Franklin |Eleanor ).*Roosevelt/ig; // Change this line
 let result = false; // Change this line
 // After passing the challenge experiment with myString and see how the grouping works
+
+//have to use .* for middle names after using the (a|b) to check for either or
+
+// make sure to use spaces after checking the names as it will search for spaces as well ex: (a |b )
+
+// solution
+
+// (Franklin |Eleanor ).*Roosevelt/ig
+// no space needed before the name ^^^ only after
+
+// the g checks all and the i makes sure it doesn't matter if it's upper or lowercase
+
+// to write a test its: let result = myRegex.test(myString); | better example for general ex: let result = a.test(b);
+// testing the regex against the string essentially
