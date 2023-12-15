599501c84e34788c484d1b4902ca662614402200
Jack Ault <henlowgg@outlook.com>
2023-12-15 01:08:45 -0500

    uh, solution

---
+++ Debugging/Catch Misspelled Variable and Function Names/CatchMisspelled.js
@@ -0,0 +1,6 @@
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
+
+// kinda lame, but just fix the spelling error, prob just wants to see if you have working brain cells to see spelling errors idk
