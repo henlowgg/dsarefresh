45e39a6ded5132f6bb64cb47d4ec1d5f7b50532b
Jack Ault <henlowgg@outlook.com>
2023-12-15 16:14:39 -0500

    solution

--- Basic Data Structures/Copy Array Items Using slice()/CopyArrayUsingSplice.js
+++ Basic Data Structures/Copy Array Items Using slice()/CopyArrayUsingSplice.js
@@ -1,8 +1,15 @@
 function forecast(arr) {
     // Only change code below this line
   
-    return arr;
+    return arr.slice(2, 4);
   }
   
   // Only change code above this line
   console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+// note that slice never modifies the original array, it just copies it and spits out a new array depending on the parameters passed
+
+// slice only takes 2 parameters
+
+// pass in the parameters (2, 4) in the slice method and return it
+
