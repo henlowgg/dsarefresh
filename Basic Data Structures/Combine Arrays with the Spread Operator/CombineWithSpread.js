ac328d01508df88a8a330907dabdf2ad9872b6b9
Jack Ault <henlowgg@outlook.com>
2023-12-16 15:56:00 -0500

    solution using spread operator to combine arrays

--- Basic Data Structures/Combine Arrays with the Spread Operator/CombineWithSpread.js
+++ Basic Data Structures/Combine Arrays with the Spread Operator/CombineWithSpread.js
@@ -1,7 +1,11 @@
 function spreadOut() {
     let fragment = ['to', 'code'];
-    let sentence; // Change this line
+    let sentence = ['learning', ...fragment, 'is', 'fun']; // Change this line
     return sentence;
   }
   
   console.log(spreadOut());
+
+// ...fragment into the array in sentence 
+
+// 'learning', ...here, 'is', 'fun'
