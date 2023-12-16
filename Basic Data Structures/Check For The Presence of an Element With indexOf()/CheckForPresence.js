247e466a8e11c98d8aa8d551a76280dc7aa55ff9
Jack Ault <henlowgg@outlook.com>
2023-12-16 15:57:32 -0500

    setup

---
+++ Basic Data Structures/Check For The Presence of an Element With indexOf()/CheckForPresence.js
@@ -0,0 +1,7 @@
+function quickCheck(arr, elem) {
+    // Only change code below this line
+  
+    // Only change code above this line
+  }
+  
+  console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
