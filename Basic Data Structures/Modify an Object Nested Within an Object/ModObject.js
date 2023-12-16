0a3754b5974e2a221deb0063d82c9e363ac1b986
Jack Ault <henlowgg@outlook.com>
2023-12-16 16:56:41 -0500

    solution by using dot notation to modify the object with a new value

--- Basic Data Structures/Modify an Object Nested Within an Object/ModObject.js
+++ Basic Data Structures/Modify an Object Nested Within an Object/ModObject.js
@@ -8,7 +8,13 @@ let userActivity = {
   };
   
   // Only change code below this line
-  
+  userActivity.data.online = 45;
+  // now online will show the value of 45 in the console instead of 42
   // Only change code above this line
   
   console.log(userActivity);
+
+  // basically just use dot notation to change the value of data to 45
+
+  // cascades from top down when using dot notation, just find the correct key to use when updating a value
+
