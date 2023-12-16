0869311bd83506e2833851ff82821a62c95f565d
Jack Ault <henlowgg@outlook.com>
2023-12-16 16:51:30 -0500

    solution, using 2 versions of bracket notation and 1 where assigning a variable to the object using dot notation

--- Basic Data Structures/Add Key-Value Pairs to JavaScript Objects/AddKeyValues.js
+++ Basic Data Structures/Add Key-Value Pairs to JavaScript Objects/AddKeyValues.js
@@ -5,7 +5,19 @@ const foods = {
   };
   
   // Only change code below this line
-  
+  // first method by assigning bananas to the object foods using .dot notation
+  foods.bananas = 13;
+
+  // bracket notation without quotes hence making it a variable where the name of the property will be whatever the value the variable is
+  const grapes = 'grapes';
+  foods[grapes] = 35;
+
+  // third method is just using straight up bracket notation 
+  // bracket notation is necessary if your property has space for it or if you want to use a variable to name the property
+  foods['strawberries'] = 27;
+
   // Only change code above this line
   
   console.log(foods);
+
+  // will implement all 3 ways to use it, just becuase why not
