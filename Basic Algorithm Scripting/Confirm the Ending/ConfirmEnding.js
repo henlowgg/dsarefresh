e4e355848d17fc74752f9a0894fa42d8c3b5365c
Jack Ault <henlowgg@outlook.com>
2023-12-18 16:28:57 -0500

    returning str.slice(-target.length) === target ; will give us the solution, it makes it so that the string is sorted by hitting it from the back type

--- Basic Algorithm Scripting/Confirm the Ending/ConfirmEnding.js
+++ Basic Algorithm Scripting/Confirm the Ending/ConfirmEnding.js
@@ -1,5 +1,8 @@
 function confirmEnding(str, target) {
-    return str;
+    return str.slice(-target.length) === target
   }
   
   confirmEnding("Bastian", "n");
+
+// Code Explanation:
+// If a negative number is provided as the first parameter to slice() , the offset is taken backwards from the end of the string.
