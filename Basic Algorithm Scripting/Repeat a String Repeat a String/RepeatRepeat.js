de9eb61fd01d3fad1645e8e0cb3c78e82cff8d8b
Jack Ault <henlowgg@outlook.com>
2023-12-18 16:36:04 -0500

    repeat a string, just use a for loop to repeat the string as many times as necessary, after declaring a variable for the new string to be stored in

---
+++ Basic Algorithm Scripting/Repeat a String Repeat a String/RepeatRepeat.js
@@ -0,0 +1,16 @@
+function repeatStringNumTimes(str, num) {
+    let repeatStr = "";
+    for (let i = 0; i < num; i++) {
+        repeatStr += str;
+    }
+    
+    return repeatStr;
+  }
+  
+  repeatStringNumTimes("abc", 3);
+
+
+  // need to write a for loop 
+
+  // also need to declare a variable for the new array to be stored in 
+
