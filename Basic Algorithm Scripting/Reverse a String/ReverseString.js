1debf31799ae9727044b72f76e343db071466317
Jack Ault <henlowgg@outlook.com>
2023-12-17 20:05:50 -0500

    solution, used .split().reverse().join() methods and chained it together

---
+++ Basic Algorithm Scripting/Reverse a String/ReverseString.js
@@ -0,0 +1,16 @@
+function reverseString(str) {
+    return str.split("").reverse("").join("");
+  }
+  
+  reverseString("hello");
+
+
+  // split reverse join
+
+  // pretty much standard for reversing a string, 
+
+  // two other ways it can be done as well, one being a for loop, and the other i think is recursive?
+
+// The split() method splits a String object into an array of string by separating the string into sub strings.
+// The reverse() method reverses an array in place. The first array element becomes the last and the last becomes the first.
+// The join() method joins all elements of an array into a string.
