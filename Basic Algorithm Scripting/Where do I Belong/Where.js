baf90a92ade9976c4c2339b45fd372b2b3fd9e5e
Jack Ault <henlowgg@outlook.com>
2023-12-18 18:26:35 -0500

    solution for where do i belong with additional solutions in the md doc

--- Basic Algorithm Scripting/Where do I Belong/Where.js
+++ Basic Algorithm Scripting/Where do I Belong/Where.js
@@ -1,5 +1,14 @@
 function getIndexToIns(arr, num) {
-    return num;
+    arr.sort((a, b) => a - b);
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= num) return i;
+    }
+    return arr.length;
   }
   
   getIndexToIns([40, 60], 50);
+
+  // first need to sort the array from lowest to biggest 
+  // once sorted, check for the first number that is bigger and return the index (where it's located on the array)
+  // if there is no index for that number then deal with that case as well
