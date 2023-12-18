1b869d4752b604eacc2cb106c7e3e347344358bc
Jack Ault <henlowgg@outlook.com>
2023-12-18 17:19:49 -0500

    solution for boo who, by using typeof method to check if whatever is passed is a boolean and returns true or false depending on the argument passed into the function

---
+++ Basic Algorithm Scripting/Boo who/BooWho.js
@@ -0,0 +1,13 @@
+function booWho(bool) {
+    return typeof bool === "boolean";
+  }
+  
+  booWho(null);
+
+
+// just use typeof method
+
+
+// typeof bool === "boolean";
+
+// will return true or false against all test cases as it strictly checks if it is a boolean
