8665df3e542d7d3e53e100ce50eea5d2429941ce
Jack Ault <henlowgg@outlook.com>
2023-12-18 19:41:19 -0500

    added a couple comments just for additional clarity and best practice for interview prep so I know what tf i'm talking about

--- Object Oriented Programming (OOP)/Understand Own Properties/OwnProps.js
+++ Object Oriented Programming (OOP)/Understand Own Properties/OwnProps.js
@@ -7,10 +7,12 @@ function Bird(name) {
   let ownProps = [];
   // Only change code below this line
 
-  // make a for loop
 
+  // make a for loop
   for (let property in canary) {
+    // check if canary has its own property
     if (canary.hasOwnProperty(property)) {
+        // push the property to the new array ownProps
         ownProps.push(property);
     }
   }
