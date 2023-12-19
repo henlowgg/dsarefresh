7127641ce3be9cf9641dcada9ccebc9e3c9c6419
Jack Ault <henlowgg@outlook.com>
2023-12-18 19:14:17 -0500

    solution

--- Object Oriented Programming (OOP)/Use a Constructor to Create Objects/UseConstructor.js
+++ Object Oriented Programming (OOP)/Use a Constructor to Create Objects/UseConstructor.js
@@ -4,3 +4,11 @@ function Dog() {
     this.numLegs = 4;
   }
   // Only change code below this line
+
+  let hound = new Dog();
+
+  // just assigning the new object 'hound' to Dog to create a new instance of 'Dog'
+
+  // important to note that 'this' inside the constructor always refers to the object being created
+
+  
