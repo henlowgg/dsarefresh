83a9a7717864eb8428fd18d26ffaa61fe13a3fe0
Jack Ault <henlowgg@outlook.com>
2023-12-19 15:35:58 -0500

    solution

--- Object Oriented Programming (OOP)/Inherit Behaviors from a Supertype/InheritBehaviors.js
+++ Object Oriented Programming (OOP)/Inherit Behaviors from a Supertype/InheritBehaviors.js
@@ -9,5 +9,9 @@ Animal.prototype = {
 
 // Only change code below this line
 
-let duck; // Change this line
-let beagle; // Change this line
+let duck = Object.create(Animal.prototype); // Change this line
+let beagle = Object.create(Animal.prototype); // Change this line
+
+
+// Object.create(obj) creates a new object and sets obj as the new objects prototype
+// so here we would use Object.create(Animal.prototype);
