caf6b3f309566a13088f3a63aa59426828d53e54
Jack Ault <henlowgg@outlook.com>
2023-12-19 14:41:36 -0500

    solution for setting the constructor property when changijng the prototype

--- Object Oriented Programming (OOP)/Remember to Set the Constructor Property when Changing the Prototype/SetConstructor.js
+++ Object Oriented Programming (OOP)/Remember to Set the Constructor Property when Changing the Prototype/SetConstructor.js
@@ -4,7 +4,7 @@ function Dog(name) {
   
   // Only change code below this line
   Dog.prototype = {
-  
+    constructor: Dog,
     numLegs: 4,
     eat: function() {
       console.log("nom nom nom");
@@ -13,3 +13,9 @@ function Dog(name) {
       console.log("My name is " + this.name);
     }
   };
+
+
+
+  // just make sure to specify the constructor in the function before continuing 
+
+  // whenever a prototype is manually set to a new object, you have to define the constructor property
