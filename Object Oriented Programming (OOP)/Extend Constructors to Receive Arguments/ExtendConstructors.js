71ca89a56bf06e4667711f5e83c831cfa00d8ab6
Jack Ault <henlowgg@outlook.com>
2023-12-18 19:28:28 -0500

    solution for extending constructors

--- Object Oriented Programming (OOP)/Extend Constructors to Receive Arguments/ExtendConstructors.js
+++ Object Oriented Programming (OOP)/Extend Constructors to Receive Arguments/ExtendConstructors.js
@@ -1,3 +1,14 @@
-function Dog() {
-
+function Dog(name, color) {
+    this.name = name;
+    this.color = color;
+    // just as a reminder, this this.numLegs will never change unless specified, its a static variable fixed at 4
+    this.numLegs = 4;
 }
+
+// creates a new Dog saved in a variable terrier
+let terrier = new Dog();
+// passes the argument 'shitter' as the new name
+terrier.name = 'shitter';
+// passes the argument 'shit brown' as the new color
+terrier.color = 'shit brown';
+
