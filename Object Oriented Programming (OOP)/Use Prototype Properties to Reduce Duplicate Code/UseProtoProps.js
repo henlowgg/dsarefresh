5cbc440148c81bb77bb2ccbca5ca7abba458cb24
Jack Ault <henlowgg@outlook.com>
2023-12-18 19:47:03 -0500

    solution for using prototype props to reduce duplicate code

--- Object Oriented Programming (OOP)/Use Prototype Properties to Reduce Duplicate Code/UseProtoProps.js
+++ Object Oriented Programming (OOP)/Use Prototype Properties to Reduce Duplicate Code/UseProtoProps.js
@@ -1,8 +1,12 @@
 function Dog(name) {
     this.name = name;
+    Dog.prototype.numLegs = 4;
   }
-  
   
   
   // Only change code above this line
   let beagle = new Dog("Snoopy");
+
+  // can place the prototype property inside or outside of the function itself
+
+  // prototype is meant to get rid of the possiblity of multiple instances of a duplicated variable
