c5cec1bd736e2e71761e6123ed39b0fa39fa88e4
Jack Ault <henlowgg@outlook.com>
2023-12-18 19:53:51 -0500

    solution for iterating over all properties

--- Object Oriented Programming (OOP)/Iterate Over All Properties/IterateProps.js
+++ Object Oriented Programming (OOP)/Iterate Over All Properties/IterateProps.js
@@ -10,3 +10,14 @@ function Dog(name) {
   let prototypeProps = [];
   
   // Only change code below this line
+
+  // write a for loop to add all of the own properties of beagle to the array ownProps
+  // use an ifelse statement to add either the properties to beagle or to add all the prototype properties of Dog to the array prototypeProps
+
+  for (let property in beagle) {
+    if (beagle.hasOwnProperty(property)) {
+        ownProps.push(property);
+    } else {
+        prototypeProps.push(property);
+    }
+  }
