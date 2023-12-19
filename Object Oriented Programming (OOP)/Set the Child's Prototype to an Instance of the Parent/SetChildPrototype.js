2b24f7076cf2ef65652575246497440087ba6094
Jack Ault <henlowgg@outlook.com>
2023-12-19 15:41:10 -0500

    solution

--- Object Oriented Programming (OOP)/Set the Child's Prototype to an Instance of the Parent/SetChildPrototype.js
+++ Object Oriented Programming (OOP)/Set the Child's Prototype to an Instance of the Parent/SetChildPrototype.js
@@ -10,6 +10,7 @@ Animal.prototype = {
 function Dog() { }
 
 // Only change code below this line
-
-
+Dog.prototype = Object.create(Animal.prototype);
+// now beagle inhjerits all of Animal prototypes including the eat method
+// just means it will inherit it without a problem now
 let beagle = new Dog();
