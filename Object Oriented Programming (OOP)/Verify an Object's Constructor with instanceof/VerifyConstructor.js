f12215b6ac3b7784f5b43c3d61b9818249b60d23
Jack Ault <henlowgg@outlook.com>
2023-12-18 19:34:00 -0500

    solution for verifying a constructor

--- Object Oriented Programming (OOP)/Verify an Object's Constructor with instanceof/VerifyConstructor.js
+++ Object Oriented Programming (OOP)/Verify an Object's Constructor with instanceof/VerifyConstructor.js
@@ -3,3 +3,8 @@ function House(numBedrooms) {
   }
   
   // Only change code below this line
+
+  // create new instance of House constructor
+
+  let myHouse = new House();
+  myHouse instanceof House;
