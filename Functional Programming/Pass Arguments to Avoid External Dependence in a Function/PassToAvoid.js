f1e43b01a723b0dfeed8ebd47086e4aefea0ca1d
Jack Ault <henlowgg@outlook.com>
2023-12-21 03:04:33 -0500

    solution

--- Functional Programming/Pass Arguments to Avoid External Dependence in a Function/PassToAvoid.js
+++ Functional Programming/Pass Arguments to Avoid External Dependence in a Function/PassToAvoid.js
@@ -2,8 +2,9 @@
 let fixedValue = 4;
 
 // Only change code below this line
-function incrementer() {
+function incrementer(value) {
 
+    return value + 1;
 
   // Only change code above this line
 }
