161fc0275286a615bbee25c4a3f2045234dfd018
Jack Ault <henlowgg@outlook.com>
2023-12-21 12:54:03 -0500

    solution

--- Functional Programming/Implement the filter Method on a Prototype/ImplementFilter.js
+++ Functional Programming/Implement the filter Method on a Prototype/ImplementFilter.js
@@ -1,7 +1,24 @@
 Array.prototype.myFilter = function(callback) {
     const newArray = [];
     // Only change code below this line
-  
+    for (let i = 0; i < this.length; i++) {
+        if (Boolean(callback(this[i], i, this)) === true) {
+            newArray.push(this[i]);
+        }
+    }
     // Only change code above this line
     return newArray;
   };
+
+  // prob prefer to use the shorter way with a double == sign and omitting the Boolean expression
+
+
+Array.prototype.myFilter = function(callback) {
+    const newArray = [];
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this) == true) {
+            newArray.push(this[i]);
+        }
+    }
+    return newArray;
+};
