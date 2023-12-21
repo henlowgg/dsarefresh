f79532b19f97c7c52185f8a1df6b5641aafe7316
Jack Ault <henlowgg@outlook.com>
2023-12-20 23:26:40 -0500

    solution with explanation of how the prototype chain of penguin actually looks like

--- Object Oriented Programming (OOP)/Override Inherited Methods/OverrideInherited.js
+++ Object Oriented Programming (OOP)/Override Inherited Methods/OverrideInherited.js
@@ -8,9 +8,27 @@ Penguin.prototype.constructor = Penguin;
 
 // Only change code below this line
 
-
+Penguin.prototype.fly = function() {
+    return "Alas, this is a flightless bird."
+}
 
 // Only change code above this line
 
 let penguin = new Penguin();
 console.log(penguin.fly());
+
+
+/* 
+by having the function:
+let penguin = new Penguin();
+
+and by calling:
+penguin.fly()
+
+this is how JS would look for the method on the prototype chain of penguin
+
+penguin => Is fly() defined here? No
+Penguin => is fly() defined here? Yes. Execute it and stop searching.
+Bird => fly() is also defined, but JS stopped searching before reaching this level.
+Object => JavaScript stopped searching before reaching this level.
+ */
