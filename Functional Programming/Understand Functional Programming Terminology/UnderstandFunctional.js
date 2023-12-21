8352ca846c109b657acff3a0bfa438552cd1aecc
Jack Ault <henlowgg@outlook.com>
2023-12-21 02:42:04 -0500

    solution

--- Functional Programming/Understand Functional Programming Terminology/UnderstandFunctional.js
+++ Functional Programming/Understand Functional Programming Terminology/UnderstandFunctional.js
@@ -20,8 +20,14 @@ const getTea = (prepareTea, numOfCups) => {
 };
 
 // Only change code below this line
-const tea4GreenTeamFCC = null;
-const tea4BlackTeamFCC = null;
+/* 
+we pass the functions prepareGreenTea() and prepareBlackTea() as parameters or callback functions
+for the getTea() functions being assigned to the two constant variables tea4BlackTeamFCC and tea4GreenTeamFCC
+This makes it so that no global variables are changed, and we have the option to add an unlimited number
+of different choices of preparedTea() methods since it's a callback function being passed to the higher order function of getTea()
+*/
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
 // Only change code above this line
 
 console.log(
