29625200aa19d2053892399d0d6b038bb2f04690
Jack Ault <henlowgg@outlook.com>
2023-12-20 23:43:10 -0500

    pseudocode

--- Object Oriented Programming (OOP)/Use Closure to Protect Properties Within an Object from Being Modified Externally/UseClosure.js
+++ Object Oriented Programming (OOP)/Use Closure to Protect Properties Within an Object from Being Modified Externally/UseClosure.js
@@ -5,3 +5,10 @@ function Bird() {
   }
 
   // basically making it so that we are making public properties private
+
+  /* 
+  need to change the code in line 2 to become a variable, not a this.prop
+
+  declare weight = 15
+  create method getWeight and return weight within the function
+  */
