1b8dab4f6042bf1f35028184501ce64e431f3b67
Jack Ault <henlowgg@outlook.com>
2023-12-21 11:56:15 -0500

    solution

--- Functional Programming/Refactor Global Variables Out of Functions/RefactorGlobalVar.js
+++ Functional Programming/Refactor Global Variables Out of Functions/RefactorGlobalVar.js
@@ -1,31 +1,48 @@
-// the global variable
-var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+// // the global variable
+// var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
 
-/* This function should add a book to the list and return the list */
-// New parameters should come before bookName
+// /* This function should add a book to the list and return the list */
+// // New parameters should come before bookName
 
-// Add your code below this line
-function add(arr, bookName) {
-  let newArr = [...arr]; // Copy the bookList array to a new array.
-  newArr.push(bookName); // Add bookName parameter to the end of the new array.
-  return newArr; // Return the new array.
+// // Add your code below this line
+// function add(arr, bookName) {
+//   let newArr = [...arr]; // Copy the bookList array to a new array.
+//   newArr.push(bookName); // Add bookName parameter to the end of the new array.
+//   return newArr; // Return the new array.
+// }
+
+// /* This function should remove a book from the list and return the list */
+// // New parameters should come before the bookName one
+
+// // Add your code below this line
+// function remove(arr, bookName) {
+//   let newArr = [...arr]; // Copy the bookList array to a new array.
+//   if (newArr.indexOf(bookName) >= 0) {
+//     // Check whether the bookName parameter is in new array.
+//     newArr.splice(newArr.indexOf(bookName), 1); // Remove the given paramater from the new array.
+//     return newArr; // Return the new array.
+//   }
+// }
+
+// var newBookList = add(bookList, 'A Brief History of Time');
+// var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+// var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+// console.log(bookList);
+
+/* The much simpler version below */
+
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add(list, bookName) {
+  return [...list, bookName];
+  // Change code above this line
 }
 
-/* This function should remove a book from the list and return the list */
-// New parameters should come before the bookName one
-
-// Add your code below this line
-function remove(arr, bookName) {
-  let newArr = [...arr]; // Copy the bookList array to a new array.
-  if (newArr.indexOf(bookName) >= 0) {
-    // Check whether the bookName parameter is in new array.
-    newArr.splice(newArr.indexOf(bookName), 1); // Remove the given paramater from the new array.
-    return newArr; // Return the new array.
-  }
+// Change code below this line
+function remove(list, bookName) {
+  return list.filter(book => book !== bookName);
+    // Change code above this line
 }
-
-var newBookList = add(bookList, 'A Brief History of Time');
-var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
-var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
-
-console.log(bookList);
