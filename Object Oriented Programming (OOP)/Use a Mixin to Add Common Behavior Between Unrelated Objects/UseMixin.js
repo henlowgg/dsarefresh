10e9f74c60d70b32d9772bd32033137e1137686d
Jack Ault <henlowgg@outlook.com>
2023-12-20 23:37:07 -0500

    solution pseudocode

--- Object Oriented Programming (OOP)/Use a Mixin to Add Common Behavior Between Unrelated Objects/UseMixin.js
+++ Object Oriented Programming (OOP)/Use a Mixin to Add Common Behavior Between Unrelated Objects/UseMixin.js
@@ -9,3 +9,24 @@ let bird = {
   };
   
   // Only change code below this line
+
+  /* 
+  We want to create a mixin named glideMixin as a function that defines a method named
+  glide.
+  Then use glideMixin to give both bird and boat the ability to glide.
+
+  This makes a function of glideMixin that defines the obj.glide method:
+```
+  let glideMixin = function(obj) {
+    obj.glide = function() {
+        console.log("");
+    }
+  }
+```
+  These are the functions giving both boat and bird the ability to 'glide':
+```
+  glideMixin(bird);
+  glideMixin(boat);
+```
+
+  */
