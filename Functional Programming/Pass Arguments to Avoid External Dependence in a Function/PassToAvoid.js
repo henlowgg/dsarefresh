58481daeb7b26ac3a08ada63e6a0f9e03f215a79
Jack Ault <henlowgg@outlook.com>
2023-12-21 03:04:11 -0500

    commenting

--- Functional Programming/Pass Arguments to Avoid External Dependence in a Function/PassToAvoid.js
+++ Functional Programming/Pass Arguments to Avoid External Dependence in a Function/PassToAvoid.js
@@ -7,3 +7,24 @@ function incrementer() {
 
   // Only change code above this line
 }
+
+/* 
+my initial solution wrote it out as follows:
+
+function incrementer(fixedValue, ) {
+return fixedValue + 1;
+}
+
+however the actual solution is different, since i want to pass the fixedValue into the function as a parameter
+
+written as:
+
+function incrementer(value) {
+    return value + 1;
+}
+
+var differentValue = incrementer(fixedValue); // should equal 5
+console.log(fixedValue); // should print 4
+
+
+*/
