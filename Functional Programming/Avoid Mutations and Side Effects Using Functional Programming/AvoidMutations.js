351865b279e6348c870b17965a7084a5bf6768ee
Jack Ault <henlowgg@outlook.com>
2023-12-21 02:58:07 -0500

    setup and solution for avoiding mutations and side effects using functional programming

---
+++ Functional Programming/Avoid Mutations and Side Effects Using Functional Programming/AvoidMutations.js
@@ -0,0 +1,18 @@
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+
+  return fixedValue + 1;
+
+  // Only change code above this line
+}
+
+// initially wanted to use a for loop out of habit, but you only need to return the variable
+
+// fixedValue and increment (+) by 1 (1)
+
+// return fixedValue + 1;
+
+// using the ++ operator on fixedValue will mutatue fixedValue, meaning it will no longer reference the same value it was assigned with
