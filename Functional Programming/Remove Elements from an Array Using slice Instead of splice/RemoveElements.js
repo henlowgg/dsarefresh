3a4bb31a14360b93069c41cdfb32990206578a94
Jack Ault <henlowgg@outlook.com>
2023-12-22 19:20:21 -0500

    solution for slice splice

--- Functional Programming/Remove Elements from an Array Using slice Instead of splice/RemoveElements.js
+++ Functional Programming/Remove Elements from an Array Using slice Instead of splice/RemoveElements.js
@@ -1,4 +1,14 @@
 function nonMutatingSplice(cities) {
 
-    return cities.splice(3);
+    // change splice to slice and add 0 to the beginning of the the location within the array to start the slice
+    return cities.slice(0, 3);
   }
+
+/* 
+The difference between splice and slice method is that the slice method 
+does not mutate the original array, but returns a new one
+The slice method takes 2 two arguments for the indices to begin and end the 
+slice (the end is non-inclusive)
+If you do not want to mutate the original array, you can use the slice method
+*/
+
