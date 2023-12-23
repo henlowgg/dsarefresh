2ec5aaeb4dd1fce7a0da67c76f1bd1e49f88b18f
Jack Ault <henlowgg@outlook.com>
2023-12-22 20:04:23 -0500

    setup for returning a sorted array without changing the original array

---
+++ Functional Programming/Return a Sorted Array Without Changing the Original Array/ReturnSortedWithoutChanging.js
@@ -0,0 +1,10 @@
+const globalArray = [5, 6, 3, 2, 9];
+
+function nonMutatingSort(arr) {
+  // Only change code below this line
+
+
+  // Only change code above this line
+}
+
+nonMutatingSort(globalArray);
