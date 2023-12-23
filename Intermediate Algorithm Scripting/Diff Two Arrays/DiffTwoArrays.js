d7b2347e443ebe1c80d3ddad8f2056963ea62495
Jack Ault <henlowgg@outlook.com>
2023-12-23 17:51:25 -0500

    solution

--- Intermediate Algorithm Scripting/Diff Two Arrays/DiffTwoArrays.js
+++ Intermediate Algorithm Scripting/Diff Two Arrays/DiffTwoArrays.js
@@ -1,5 +1,19 @@
 function diffArray(arr1, arr2) {
     const newArr = [];
+
+    function firstArr(first, second) {
+        // loop through array to find elements that don't exist in another array
+        for (let i = 0; i < first.length; i++) {
+            if (second.indexOf(first[i]) === -1) {
+                // pushing elements unique to first to newArr
+                newArr.push(first[i]);
+            }
+        }
+    }
+
+    firstArr(arr1, arr2);
+    firstArr(arr2, arr1);
+
     return newArr;
   }
   
