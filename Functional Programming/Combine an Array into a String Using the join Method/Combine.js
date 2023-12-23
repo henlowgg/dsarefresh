feaf79804c0911a3d6fd321d93d167e4a9f0051c
Jack Ault <henlowgg@outlook.com>
2023-12-22 20:57:28 -0500

    solution

--- Functional Programming/Combine an Array into a String Using the join Method/Combine.js
+++ Functional Programming/Combine an Array into a String Using the join Method/Combine.js
@@ -1,8 +1,14 @@
 function sentensify(str) {
     // Only change code below this line
   
+        return str.split(/\W/).join(" ");
   
     // Only change code above this line
   }
   
   sentensify("May-the-force-be-with-you");
+
+  /* 
+  - Convert the string to an array first
+  - Use Regex to split the string
+  */
