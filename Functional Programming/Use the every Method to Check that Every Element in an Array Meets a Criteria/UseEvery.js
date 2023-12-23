c34db58b0c1f52148ace7c9a11561349b1450c85
Jack Ault <henlowgg@outlook.com>
2023-12-22 21:25:14 -0500

    setup for using Some Method to check that any elements in an array meet a criteria

--- Functional Programming/Use the every Method to Check that Every Element in an Array Meets a Criteria/UseEvery.js
+++ Functional Programming/Use the every Method to Check that Every Element in an Array Meets a Criteria/UseEvery.js
@@ -1,10 +1,31 @@
 function checkPositive(arr) {
     // Only change code below this line
   
+    return arr.every(val => val > 0);
   
     // Only change code above this line
   }
   
   checkPositive([1, 2, 3, -4, 5]);
 
-  /*  */
+  /* either double return, or check in single line with conditional function => */
+
+  // for the purpose of clarity, ill write out my initial answer, and what i would prefer to have done
+
+
+  /* 
+  function checkPositive(arr) {
+    return arr.every(function(currentValue) {
+        return currentValue > 0;
+    });
+  };
+
+  checkPositive([1, 2, 3, -4, 5]);
+  */
+
+  /* 
+  function checkPositive(arr) {
+    return arr.every(val => val > 0);
+  }
+  checkPositive([1, 2, 3, -4, 5]);
+  */
