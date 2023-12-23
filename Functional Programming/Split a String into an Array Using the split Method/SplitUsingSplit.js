d3a0188d50ba49a46e7ee2feed31195ab0ef2ea3
Jack Ault <henlowgg@outlook.com>
2023-12-22 20:52:11 -0500

    solution

--- Functional Programming/Split a String into an Array Using the split Method/SplitUsingSplit.js
+++ Functional Programming/Split a String into an Array Using the split Method/SplitUsingSplit.js
@@ -1,8 +1,14 @@
 function splitify(str) {
     // Only change code below this line
-  
+    return str.split(/\W/)
   
     // Only change code above this line
   }
   
   splitify("Hello World,I-am code");
+
+  /* Need to use Regex to solve it
+  - str.split(/\W/)
+    - Additionally, for clarity, `/\W/` Matches any non-word character. This includes spaces and punctuation, but not underscores.
+    - /\W/ is equivalent to /[^A-Za-z0-9_]/
+  */
