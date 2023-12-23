76ce59c5d41573316cd83038b4c447d7e1655ba6
Jack Ault <henlowgg@outlook.com>
2023-12-22 21:29:22 -0500

    solution, using a conditional return statement to check if the nums in a given array are positive (true) or negative (false)

--- Functional Programming/Use the some Method to Check that Any Elements in an Array Meet a Criteria/SomeMethod.js
+++ Functional Programming/Use the some Method to Check that Any Elements in an Array Meet a Criteria/SomeMethod.js
@@ -1,8 +1,16 @@
 function checkPositive(arr) {
     // Only change code below this line
   
+    return arr.some(val => val > 0);
   
     // Only change code above this line
   }
   
   checkPositive([1, 2, 3, -4, 5]);
+
+  // checks if its true or not, ergo boolean expression
+
+
+/* 
+Similar to last excercise, we need to return arr.some with either a double return statement or a conditional return function checking if the nums are greater than 0 (checks if num is positive)
+*/
