17a564c25f0282f2eda4b3b3cf5a51b5eb5f2bf1
Jack Ault <henlowgg@outlook.com>
2023-12-22 20:03:19 -0500

    solution

--- Functional Programming/Sort an Array Alphabetically using the sort Method/SortArrayUsingSort.js
+++ Functional Programming/Sort an Array Alphabetically using the sort Method/SortArrayUsingSort.js
@@ -1,7 +1,9 @@
 function alphabeticalOrder(arr) {
     // Only change code below this line
   
-    return arr
+    return arr.sort(function(a, b) {
+      return a === b ? 0 : a < b ? -1 : 1;
+    });
     // Only change code above this line
   }
   
