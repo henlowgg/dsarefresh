eec76e1cefb3793098d9ec35c891b0f09d986388
Jack Ault <henlowgg@outlook.com>
2023-12-23 19:03:32 -0500

    solution

--- Intermediate Algorithm Scripting/Wherefore art thou/WhereUAt.js
+++ Intermediate Algorithm Scripting/Wherefore art thou/WhereUAt.js
@@ -1,5 +1,18 @@
 function whatIsInAName(collection, source) {
+    const collectionMatches = [];
 
+    for (let i = 0; i < collection.length; i++) {
+        let foundMisMatch = false;
+            for (const sourceProp in source) {
+                if (collection[i][sourceProp] !== source[sourceProp]) {
+                    foundMisMatch = true;
+                }
+            }
+        if (!foundMisMatch) {
+            collectionMatches.push(collection[i]);
+        }
+    }
+    return collectionMatches;
 }
 
 whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
