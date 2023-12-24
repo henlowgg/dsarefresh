8eb1727b9a33c583525bd4a6c154eb04020cf0f3
Jack Ault <henlowgg@outlook.com>
2023-12-23 19:16:15 -0500

    solution

--- Intermediate Algorithm Scripting/Pig Latin/PigLatin.js
+++ Intermediate Algorithm Scripting/Pig Latin/PigLatin.js
@@ -1,5 +1,11 @@
 function translatePigLatin(str) {
-    return str;
+    let consRegex = /^[^aeiou]+/;
+    let myCons = str.match(consRegex);
+
+    return myCons !== null
+        ? str
+        .replace(consRegex, "").concat(myCons).concat("ay")
+        : str.concat("way");
   }
   
   translatePigLatin("consonant");
