332a566fff480bd86f0c4d8e9b509970fa0b3e97
Jack Ault <henlowgg@outlook.com>
2023-12-28 18:45:06 -0500

    solution

--- Intermediate Algorithm Scripting/Drop it/DropIt.js
+++ Intermediate Algorithm Scripting/Drop it/DropIt.js
@@ -1,5 +1,11 @@
 function dropElements(arr, func) {
+    while (arr.length > 0 && !func(arr[0])) {
+      arr.shift();
+    }
     return arr;
   }
   
-  dropElements([1, 2, 3], function(n) {return n < 3; });
+  // test here
+  dropElements([1, 2, 3, 4], function(n) {
+    return n >= 3;
+  });
