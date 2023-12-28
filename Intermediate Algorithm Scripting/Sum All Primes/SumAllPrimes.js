54b0764957d581fb73533ada4b1e3c4f317a0a0c
Jack Ault <henlowgg@outlook.com>
2023-12-28 18:42:53 -0500

    solution

--- Intermediate Algorithm Scripting/Sum All Primes/SumAllPrimes.js
+++ Intermediate Algorithm Scripting/Sum All Primes/SumAllPrimes.js
@@ -1,5 +1,25 @@
 function sumPrimes(num) {
-    return num;
-  }
+    // Helper function to check primality
+    function isPrime(num) {
+      const sqrt = Math.sqrt(num);
+      for (let i = 2; i <= sqrt; i++) {
+        if (num % i === 0)
+          return false;
+      }
+      return true;
+    }
   
-  sumPrimes(10);
+    // Check all numbers for primality
+    let sum = 0;
+    for (let i = 2; i <= num; i++) {
+      if (isPrime(i))
+        sum += i;
+    }
+    return sum;
+  }
+
+  /* 
+  Code Explanation
+We loop over all values in our range, adding them to the sum if they are prime.
+Our primality checking function returns false if the target number is divisible by any number in between 2 and the square root of the target number. We only need to check up to the square root because the square root of a number is the largest possible unique divisor.
+  */
