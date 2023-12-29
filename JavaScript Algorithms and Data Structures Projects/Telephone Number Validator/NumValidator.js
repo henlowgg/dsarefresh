f39d26e4023b87e8c29542a1af12da9fd36e6ede
Jack Ault <henlowgg@outlook.com>
2023-12-28 21:06:45 -0500

    solution and comments

--- JavaScript Algorithms and Data Structures Projects/Telephone Number Validator/NumValidator.js
+++ JavaScript Algorithms and Data Structures Projects/Telephone Number Validator/NumValidator.js
@@ -1,5 +1,29 @@
 function telephoneCheck(str) {
-    return true;
-  }
-  
-  telephoneCheck("555-555-5555");
+    // Regular expression for validating US phone numbers
+    const regex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s\-]?)\d{3}([\s\-]?)\d{4}$/;
+
+    return regex.test(str);
+}
+
+// Test the function with various phone number formats
+console.log(telephoneCheck("555-555-5555"));    // true
+console.log(telephoneCheck("(555)555-5555"));   // true
+console.log(telephoneCheck("(555) 555-5555"));  // true
+console.log(telephoneCheck("555 555 5555"));    // true
+console.log(telephoneCheck("5555555555"));      // true
+console.log(telephoneCheck("1 555 555 5555"));  // true
+console.log(telephoneCheck("8oo-six427676;laskdjf"));  // false
+
+
+
+/* 
+
+In this regular expression:
+^(1\s?)? optionally matches the country code (1) followed by a space or nothing.
+(\(\d{3}\)|\d{3}) matches the area code either enclosed in parentheses or not.
+[\s\-]? matches an optional space or dash as a separator.
+\d{3} matches the first set of 3 digits.
+[\s\-]? matches another optional space or dash.
+\d{4}$ matches the last set of 4 digits, and $ ensures that the string ends after these digits.
+This function will return true for valid US phone numbers and false for invalid formats.
+*/
--- JavaScript Algorithms and Data Structures Projects/Telephone Number Validator/NumValidator.md
+++ JavaScript Algorithms and Data Structures Projects/Telephone Number Validator/NumValidator.md
@@ -10,3 +10,21 @@ The user may fill out the form field any way they choose as long as it has the f
 5555555555
 1 555 555 5555
 For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number based on any combination of the formats provided above. The area code is required. If the country code is provided, you must confirm that the country code is 1. Return true if the string is a valid US phone number; otherwise return false.
+
+
+
+
+
+
+
+
+
+-------
+
+
+Area code: It can be just 3 digits, or enclosed in parentheses.
+First separator: It can be a space, a dash, or no separator at all.
+First set of 3 digits: Following the area code.
+Second separator: Same as the first separator.
+Last set of 4 digits: Concluding the phone number.
+Country code (optional): If present, it must be 1, followed by a space or a dash.
